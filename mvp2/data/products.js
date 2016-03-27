f0ef60982cda0654597a2392201e6a12fa26d6dd
Elad Katz <eladkt@gmail.com>
2016-03-28 00:17:33 +0300

    first base!

--- mvp2/data/products.js
+++ mvp2/data/products.js
@@ -1,44 +1,37 @@
 
-var products =
+var productTypes = ['fixedPrice', 'perDuration', 'perMonthlyUsage'];
 
+var products =
 [
     {price:39,	category:"טלפון נייח",	name:"חבילת בנק 002 דקות יעדים ניידים"},
-    {price:1,	category:"שיחות בינלאומיות",	name:"סינגפור מבצע תקשורת בינלאומית לעסקים"},
-    {price:1,	category:"שיחות בינלאומיות",	name:"שוויץ מבצע תקשורת בינלאומית לעסקים"},
-    {price:1,	category:"שיחות בינלאומיות",	name:"שוויץ -סלולארי מבצע תקשורת בינלאומית לעסקים"},
-    {price:1,	category:"שיחות בינלאומיות",	name:"בריטניה מבצע תקשורת בינלאומית לעסקים"},
+    {price:1,	category:"שיחות בינלאומיות",	name:"סינגפור מבצע תקשורת",   type:"perDuration", columns:[2, 1]},
+    {price:1,	category:"שיחות בינלאומיות",	name:"שוויץ מבצע תקשורת",         type:"perDuration", columns:[2, 1]},
+    {price:1,	category:"שיחות בינלאומיות",	name:"שוויץ -סלולארי מבצע תקשורת", type:"perDuration", columns:[2, 1]},
+    {price:1,	category:"שיחות בינלאומיות",	name:"בריטניה מבצע תקשורת",       type:"perDuration", columns:[2, 1]},
     {price:1,	category:"שיחות בינלאומיות",	name:"בריטניה -סלולארי חבילת בנק דקות יעדים ניידים 002"},
-    {price:1,	category:"שיחות בינלאומיות",	name:"בריטניה -סלולארי חבילת בנק"},
-    {price:1,	category:"שיחות בינלאומיות",	name:"רוסיה מבצע תקשורת בינלאומית לעסקים"},
-    {price:1,	category:"שיחות בינלאומיות",	name:"רוסיה -סלולארי מבצע תקשורת בינלאומית לעסקים"},
-    {price:1,	category:"שיחות בינלאומיות",	name:"דרום אפריקה מבצע תקשורת בינלאומית לעסקים"},
-    {price:1,	category:"שיחות בינלאומיות",	name:"שוויץ מבצע תקשורת בינלאומית לעסקים"},
-    {price:1,	category:"שיחות בינלאומיות",	name:"אירלנד מבצע תקשורת בינלאומית לעסקים"},
-    {price:1,	category:"שיחות בינלאומיות",	name:"דרום אפריקה -סלולארי מבצע תקשורת בינלאומית לעסקים"},
-    {price:1,	category:"שיחות בינלאומיות",	name:"יוון מבצע תקשורת בינלאומית לעסקים"},
-    {price:1,	category:"שיחות בינלאומיות",	name:"גרמניה מבצע תקשורת בינלאומית לעסקים"},
-    {price:69,	category:"אינטרנט",name:	"40-adsl בנדל בזק 3 שנים עסקי-קיים"},
-    {price:20,	category:"אינטרנט",name:	"5-adsl- יוזר בודד"},
+    {price:1,	category:"שיחות בינלאומיות",	name:"בריטניה -סלולארי חבילת בנק", type:"perDuration", columns:[2, 1]},
+    {price:1,	category:"שיחות בינלאומיות",	name:"רוסיה מבצע תקשורת" ,        type:"perDuration", columns:[2, 1]},
+    {price:1,	category:"שיחות בינלאומיות",	name:"רוסיה -סלולארי מבצע תקשורת", type:"perDuration", columns:[2, 1]},
+    {price:1,	category:"שיחות בינלאומיות",	name:"דרום אפריקה מבצע תקשורת",   type:"perDuration", columns:[2, 1]},
+    {price:1,	category:"שיחות בינלאומיות",	name:"אירלנד מבצע תקשורת",        type:"perDuration", columns:[2, 1]},
+    {price:1,	category:"שיחות בינלאומיות",	name:"דרום אפריקה -סלולארי מבצע תקשורת", type:"perDuration", columns:[2, 1]},
+    {price:1,	category:"שיחות בינלאומיות",	name:"יוון מבצע תקשורת",           type:"perDuration", columns:[2, 1]},
+    {price:1,	category:"שיחות בינלאומיות",	name:"גרמניה מבצע תקשורת",        type:"perDuration", columns:[2, 1]},
+    {price:89,	category:"אינטרנט",name:	"lsda-04  בנדל", type:'fixedPrice', columns:[0]},
+    {price:20,	category:"אינטרנט",name:	"-adsl 40-", type:'fixedPrice', columns:[0]},
+    {price:20,	category:"אינטרנט",name:	"lsda-51-מבצע", type:'fixedPrice', columns:[0]},
+    {price:30,	category:"אינטרנט",name:	"fix ip 2.5-", type:'fixedPrice', columns:[0]},
+
+
+    {price:20,	category:"אינטרנט",name:	"5-adsl- יוזר בודד", type:'fixedPrice', columns:[0]},
     {price:0,	category:"אינטרנט",name:	"צריכת חשמל -רבע ארון 4A"},
     {price:69,	category:"אינטרנט",name:	"40-adsl בנדל בזק 3 שנים עסקי-קיים"},
     {price:20,	category:"אינטרנט",name:	"15-adsl בנדל בזק 3 שנים"},
     {price:0,	category:"אינטרנט",name:	"-Easylink EDi חודשי"},
     {price:10,	category:"אינטרנט",name:	"1G -תיבת דואר ללקוח ללא יוזר"},
     {price:0,	category:"אינטרנט",name:	"אירוח רבע ארון (12U) כולל 4 אמפר חיוב יחסי"},
-    {price:800,	category:"אינטרנט",name:	"קו תשתית בזק METRO 10M/5"},
-    {price:999, category:"debug", name: "מספר לקוח משלם"}
+    {price:800,	category:"אינטרנט",name:	"קו תשתית בזק METRO 10M/5"}
 
-    //{20,	"אינטרנט	", "שירותים מתקדמים"},
-    //{20,	"אינטרנט	", "גלישה במהירות 15 מגה כקבועה ותיבת"},
-    //{20,	"אינטרנט	", "דואר חינם גלישה במהירות 15 מגה כ.קבועה ותיבת"},
-    //{20,	"אינטרנט	", "דואר חינם גלישה במהירות 15 מגה ו.פ.ד.נ כתובת"},
-    //{20,	"אינטרנט	", "קבועה ותיבת דואר חינם גלישה במהירות 15 מגה כ.קבועה רשת"},
-    //{20,	"אינטרנט	", "קבועה ותיבת דואר חינם גלישה במהירות15מגה ו.פ.ד.נ כתובת"},
-    //{20,	"אינטרנט	", "קבועה ותיבת דואר חינם גלישה במהירות 15 מגה כ.קבועה ותיבת"},
-    //{20,	"אינטרנט	", "דואר חינם גלישה במהירות 15 מגה ו.פ.ד.נ כתובת"},
-    //{0,	"אינטרנט	", "דואר חינם"},
-    //{800,	"אינטרנט	", "קו קישור קבוע 4/4 - קו קישור קבוע"},
-    //{800,	"אינטרנט	", "קו תמסורת מטרו בזק 4/4 - קו תמסורת מטרו בזק 4/4"},
 ];
 
 module.exports = products;
